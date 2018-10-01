767e2f4f6d355c8064b499b40e9541808077b4dd
Mdewet <mathieu.dewet@gmail.com>
2018-10-01 11:13:49 +0200

    move some script instructions in sh files to improve reusability and improve before_deploy script

---
+++ ci/before_deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+echo -n ${CI_SERVICE_ACCOUNT} | base64 -d > travis-service-account.json
+gcloud auth activate-service-account ${CI_SERVICE_ACCOUNT} --key-file travis-service-account.json --project=${PROJECT_ID}
+gcloud config set compute/zone europe-west1-c
+gcloud config set core/project ${PROJECT_ID}
+gcloud container clusters get-credentials api-platform-demo --zone europe-west1-c --project ${PROJECT_ID}
+helm init --upgrade
