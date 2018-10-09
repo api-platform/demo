bdb947ff0c2be2d8edd4b09cf5e3b78589bfee37
Mdewet <mathieu.dewet@gmail.com>
2018-10-09 18:56:18 +0200

    Parrallelize admin and clients builds and buckets deployments

---
+++ ci/admin_build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"/../admin
+
+yarn && REACT_APP_API_ENTRYPOINT=https://${API_ENTRYPOINT} yarn build --environment=prod;
--- ci/admin_deploy.sh
+++ ci/admin_deploy.sh
@@ -6,8 +6,3 @@ cd "$(dirname "$0")"/..
 gsutil rsync -R admin/build gs://${ADMIN_BUCKET}
 gsutil web set -m index.html gs://${ADMIN_BUCKET}
 gsutil iam ch allUsers:objectViewer gs://${ADMIN_BUCKET}
-
-# Client build deployment to bucket
-gsutil rsync -R client/build gs://${CLIENT_BUCKET}
-gsutil web set -m index.html gs://${CLIENT_BUCKET}
-gsutil iam ch allUsers:objectViewer gs://${CLIENT_BUCKET}
