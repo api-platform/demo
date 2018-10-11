b214df9ac5ba78548f1eedbbad3c9bd2fa12f965
Mdewet <mathieu.dewet@gmail.com>
2018-10-11 18:09:28 +0200

    Update script

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -3,8 +3,8 @@
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
-echo -n "${CI_SERVICE_ACCOUNT_KEY}" | base64 -d > travis-service-account.json
-gcloud auth activate-service-account "${CI_SERVICE_ACCOUNT}" --key-file travis-service-account.json --project="${PROJECT_ID}"
+echo -n "${CI_SERVICE_ACCOUNT_KEY}" | base64 -d > ci-service-account.json
+gcloud auth activate-service-account "${CI_SERVICE_ACCOUNT}" --key-file ci-service-account.json --project="${PROJECT_ID}"
 gcloud config set compute/zone europe-west1-c
 gcloud config set core/project "${PROJECT_ID}"
 gcloud container clusters get-credentials api-platform-demo --zone europe-west1-c --project "${PROJECT_ID}"
