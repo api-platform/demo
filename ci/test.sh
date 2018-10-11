d01796dd9e9b35c8a8185ba6ebb76628cadc332b
Mdewet <mathieu.dewet@gmail.com>
2018-10-11 12:54:45 +0200

    Remove irrelevant port use check

--- ci/test.sh
+++ ci/test.sh
@@ -2,7 +2,6 @@
 
 docker-compose up -d
 helm lint api/helm/api/
-while sudo lsof -Pi :5432 -sTCP:LISTEN -t; do sleep 1; done
 docker-compose exec php composer install -o -n
 docker-compose exec php bin/console security:check
 docker-compose exec php bin/console doctrine:schema:validate --skip-sync
