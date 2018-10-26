47f4da92a2a978c69f5833895f4ce7289c802f78
Vincent CHALAMON <vincentchalamon@gmail.com>
2018-10-26 09:16:49 +0200

    Configure eslint (#56)
    * Fix #54
    
    * Pass jest with no tests

--- ci/test.sh
+++ ci/test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 docker-compose up -d
 helm lint api/helm/api/
 sleep 20
@@ -13,7 +15,8 @@ docker-compose exec php bin/console doctrine:schema:drop --env=test --force
 docker-compose exec php bin/console cache:warmup --env=test
 docker-compose exec php bin/behat --format=progress
 docker-compose exec client yarn install --pure-lockfile
-docker-compose exec client yarn jest --ci --reporters=default --reporters=jest-junit
+docker-compose exec client yarn jest --ci --reporters=default --reporters=jest-junit --passWithNoTests
+docker-compose exec client yarn eslint src
 curl -s http://localhost # Client
 curl -s http://localhost:81 # Admin
 curl -s http://localhost:8080 # API
