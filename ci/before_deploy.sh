a4d0dbeb32b163437d8fe3b4a9d7766b13db55ff
Vincent Chalamon <vincentchalamon@gmail.com>
2019-01-15 14:24:32 +0100

    Fix deploy

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -10,4 +10,4 @@ gcloud auth activate-service-account ${CI_SERVICE_ACCOUNT} --key-file travis-ser
 gcloud config set compute/zone europe-west1-c
 gcloud config set core/project ${PROJECT_ID}
 gcloud container clusters get-credentials api-platform-demo --zone europe-west1-c --project ${PROJECT_ID}
-helm init --upgrade
+helm init --upgrade --service-account tiller
--- ci/deploy.sh
+++ ci/deploy.sh
@@ -7,18 +7,20 @@ echo 'deploy script'
 helm dependencies update ./api/helm/api
 
 # You can customize it to fit your needs, for example for branch naming e.g. PHP_REPOSITORY="eu.gcr.io/${PROJECT_ID}/php-${BRANCH}"
-if [[ ${BRANCH} == "master" ]]
+if [[ ${BRANCH} == ${DEPLOYMENT_BRANCH} ]]
 then
     export PHP_REPOSITORY="eu.gcr.io/${PROJECT_ID}/php";
     export NGINX_REPOSITORY="eu.gcr.io/${PROJECT_ID}/nginx";
     export VARNISH_REPOSITORY="eu.gcr.io/${PROJECT_ID}/varnish";
-    export STATIC_IP="--set ingress.annotations.kubernetes.io/ingress.global-static-ip-name: ${STATIC_IP}";
+    export STATIC_IP="--set ingress.annotations.kubernetes.io/ingress.global-static-ip-name: demo-ip";
 else
     export PHP_REPOSITORY="eu.gcr.io/${PROJECT_ID}/php-staging";
     export NGINX_REPOSITORY="eu.gcr.io/${PROJECT_ID}/nginx-staging";
     export VARNISH_REPOSITORY="eu.gcr.io/${PROJECT_ID}/varnish-staging";
     export STATIC_IP="";
 fi
+export NAMESPACE=demo-${BRANCH};
+export RELEASE=${NAMESPACE};
 
 # Build and push the docker images.
 docker build --pull -t ${PHP_REPOSITORY} -t ${PHP_REPOSITORY}:latest api --target api_platform_php;
@@ -30,29 +32,31 @@ gcloud docker -- push ${VARNISH_REPOSITORY}:latest;
 
 # Perform a rolling update if a release in the given namespace ever exist, create one otherwise.
 # Be aware that we have the static ip for the master branch but it belongs to you to care about others.
-helm upgrade --install --reset-values --wait --force --namespace=${BRANCH} --recreate-pods demo ./api/helm/api STATIC_IP \
+helm upgrade --install --reset-values --wait --force --namespace=${NAMESPACE} --recreate-pods ${RELEASE} ./api/helm/api ${STATIC_IP} \
     --set php.repository=${PHP_REPOSITORY} \
     --set nginx.repository=${NGINX_REPOSITORY} \
     --set varnish.repository=${VARNISH_REPOSITORY} \
     --set secret=${APP_SECRET} \
     --set postgresUser=${DATABASE_USER},postgresPassword="${DATABASE_PASSWORD}",postgresDatabase=${DATABASE_NAME} --set postgresql.persistence.enabled=true;
 
-kubectl exec -it $(kubectl --namespace=${BRANCH} get pods -l app=api-php -o jsonpath="{.items[0].metadata.name}") --namespace=${BRANCH} \
-    -- ash -c'export APP_ENV=dev && composer install -n && bin/console d:s:u --force --env=dev && bin/console hautelook:fixtures:load -n && APP_ENV=prod composer --no-dev install --classmap-authoritative && bin/console d:s:u --env=prod';
+echo "Waiting for api-php to be up and ready..."
+sleep 60
+kubectl exec -it $(kubectl --namespace=${NAMESPACE} get pods -l app=api-php -o jsonpath="{.items[0].metadata.name}") --namespace=${NAMESPACE} \
+    -- sh -c 'APP_ENV=dev composer install -n && bin/console d:m:m --no-interaction --env=prod && bin/console d:s:u --force --env=prod && bin/console hautelook:fixtures:load -n --env=dev && APP_ENV=prod composer --no-dev install --classmap-authoritative && echo deployed';
 
 # For the master branch the REACT_APP_API_ENTRYPOINT will be the URL plug on your static IP.
 # For Dev branchs you can use the IP retrievable by the kubectl get ingress command.
-if [[ ${BRANCH} == "master" ]]
+if [[ ${BRANCH} == ${DEPLOYMENT_BRANCH} ]]
 then
     export API_ENTRYPOINT=${PROD_DNS};
 else
-    export API_ENTRYPOINT=$(kubectl --namespace `echo ${BRANCH}` get ingress -o jsonpath='{.items[0].status.loadBalancer.ingress[0].ip}');
+    export API_ENTRYPOINT=$(kubectl --namespace `echo ${NAMESPACE}` get ingress -o jsonpath='{.items[0].status.loadBalancer.ingress[0].ip}');
 fi
 
 cd admin && yarn && REACT_APP_API_ENTRYPOINT=https://${API_ENTRYPOINT} yarn build --environment=prod;
 cd ../client && yarn && REACT_APP_ADMIN_HOST_HTTPS=https://${ADMIN_BUCKET} REACT_APP_ADMIN_HOST_HTTP=http://${ADMIN_BUCKET} REACT_APP_API_CACHED_HOST_HTTPS=https://${API_ENTRYPOINT} REACT_APP_API_CACHED_HOST_HTTP=http://${API_ENTRYPOINT} yarn build --environment=prod && cd ..;
 
-if [[ ${BRANCH} == "master" ]]
+if [[ ${BRANCH} == ${DEPLOYMENT_BRANCH} ]]
 then
     gsutil rsync -R admin/build gs://${ADMIN_BUCKET}
     gsutil rsync -R admin/build gs://${CLIENT_BUCKET}
