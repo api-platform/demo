807587842c1666042a7bbf5a25c20281c072e098
Alan Poulain <contact@alanpoulain.eu>
2019-02-05 12:02:13 +0100

    Custom layout for admin

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,4 +1,19 @@
 import React from 'react';
 import { HydraAdmin } from '@api-platform/admin';
+import { createMuiTheme } from '@material-ui/core/styles';
 
-export default () => <HydraAdmin entrypoint={process.env.REACT_APP_API_ENTRYPOINT}/>;
+import { Layout } from './layout';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#38a9b4',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#288690',
+    },
+  },
+});
+
+export default () => <HydraAdmin appLayout={Layout} theme={theme} entrypoint={process.env.REACT_APP_API_ENTRYPOINT}/>;
