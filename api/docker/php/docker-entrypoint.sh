dbe88cbcc445de3c944c3f5f9e00e8b5ce364212
Kévin Dunglas <dunglas@gmail.com>
2019-06-06 09:44:05 +0200

    Fix deploy

--- api/docker/php/docker-entrypoint.sh
+++ api/docker/php/docker-entrypoint.sh
@@ -18,8 +18,8 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ] || { [ "$1" = 'php' ] && [ "
 			mkdir -p config/jwt
 			echo "$jwt_passphrase" | openssl genpkey -out config/jwt/private.pem -pass stdin -aes256 -algorithm rsa -pkeyopt rsa_keygen_bits:4096
 			echo "$jwt_passphrase" | openssl pkey -in config/jwt/private.pem -passin stdin -out config/jwt/public.pem -pubout
-			setfacl -R -m u:www-data:rX -m u:"$(whoami)":rwX config/jwt || echo "setfacl failed"
-            setfacl -dR -m u:www-data:rX -m u:"$(whoami)":rwX config/jwt || echo "setfacl failed"
+			setfacl -R -m u:www-data:rX -m u:"$(whoami)":rwX config/jwt
+            setfacl -dR -m u:www-data:rX -m u:"$(whoami)":rwX config/jwt
 		fi
 	fi
 
