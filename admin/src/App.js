366957059ab0a860cee03265525daffaa3222206
Kévin Dunglas <dunglas@gmail.com>
2019-09-30 11:06:18 +0200

    Add a custom admin using the new version of APIP Admin (#128)
    * Add a custom admin using the new version of APIP Admin
    
    * Bump deps

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,19 +1,97 @@
-import React from 'react';
-import { HydraAdmin } from '@api-platform/admin';
-import { createMuiTheme } from '@material-ui/core/styles';
+import React from "react";
+import {
+  HydraAdmin,
+  ResourceGuesser,
+  ListGuesser,
+  ShowGuesser,
+  CreateGuesser,
+  EditGuesser,
+  FieldGuesser,
+  InputGuesser
+} from "@api-platform/admin";
+import { ReferenceInput, AutocompleteInput } from "react-admin";
 
-import { Layout } from './layout';
+const ReviewsList = props => (
+  <ListGuesser {...props}>
+    <FieldGuesser source="author" />
+    <FieldGuesser source="book" />
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#38a9b4',
-      contrastText: '#fff',
-    },
-    secondary: {
-      main: '#288690',
-    },
-  },
-});
+    {/* While deprecated fields are hidden by default, using an explicit FieldGuesser component allows to add them back. */}
+    <FieldGuesser source="letter" />
+  </ListGuesser>
+);
 
-export default () => <HydraAdmin appLayout={Layout} theme={theme} entrypoint={process.env.REACT_APP_API_ENTRYPOINT}/>;
+const ReviewsShow = props => (
+  <ShowGuesser {...props}>
+    <FieldGuesser source="author" addLabel={true} />
+    <FieldGuesser source="book" addLabel={true} />
+    <FieldGuesser source="rating" addLabel={true} />
+
+    {/* While deprecated fields are hidden by default, using an explicit FieldGuesser component allows to add them back. */}
+    <FieldGuesser source="letter" addLabel={true} />
+
+    <FieldGuesser source="body" addLabel={true} />
+    <FieldGuesser source="publicationDate" addLabel={true} />
+  </ShowGuesser>
+);
+
+const ReviewsCreate = props => (
+  <CreateGuesser {...props}>
+    <InputGuesser source="author" />
+    <ReferenceInput
+      source="book"
+      reference="books"
+      label="Books"
+      filterToQuery={searchText => ({ title: searchText })}
+    >
+      <AutocompleteInput optionText="title" />
+    </ReferenceInput>
+
+    <InputGuesser source="rating" />
+
+    {/* While deprecated fields are hidden by default, using an explicit InputGuesser component allows to add them back. */}
+    <InputGuesser source="letter" />
+
+    <InputGuesser source="body" />
+    <InputGuesser source="publicationDate" />
+  </CreateGuesser>
+);
+
+const ReviewsEdit = props => (
+  <EditGuesser {...props}>
+    <InputGuesser source="author" />
+
+    <ReferenceInput
+      source="book"
+      reference="books"
+      label="Books"
+      filterToQuery={searchText => ({ title: searchText })}
+    >
+      <AutocompleteInput optionText="title" />
+    </ReferenceInput>
+
+    <InputGuesser source="rating" />
+
+    {/* While deprecated fields are hidden by default, using an explicit InputGuesser component allows to add them back. */}
+    <InputGuesser source="letter" />
+
+    <InputGuesser source="body" />
+    <InputGuesser source="publicationDate" />
+  </EditGuesser>
+);
+
+export default () => (
+  <HydraAdmin entrypoint={process.env.REACT_APP_API_ENTRYPOINT}>
+    <ResourceGuesser name="books" />
+    <ResourceGuesser
+      name="reviews"
+      list={ReviewsList}
+      show={ReviewsShow}
+      create={ReviewsCreate}
+      edit={ReviewsEdit}
+    />
+
+    {/* While deprecated resources are hidden by default, using an explicit ResourceGuesser component allows to add them back. */}
+    <ResourceGuesser name="parchments" />
+  </HydraAdmin>
+);
