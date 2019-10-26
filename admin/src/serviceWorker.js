88562cc1ae2791aaa8478f7f4f63ac608c8d40ac
tokirmanva22 <38378125+tokirmanva22@users.noreply.github.com>
2019-10-26 21:34:55 +0530

    Update better code
    just reduce space in code .

--- admin/src/serviceWorker.js
+++ admin/src/serviceWorker.js
@@ -24,12 +24,12 @@ export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
-    if (publicUrl.origin !== window.location.origin) {
+    if (publicUrl.origin !== window.location.origin)
+      return;
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebook/create-react-app/issues/2374
-      return;
-    }
+      
 
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
@@ -46,10 +46,9 @@ export function register(config) {
             'worker. To learn more, visit http://bit.ly/CRA-PWA'
           );
         });
-      } else {
-        // Is not localhost. Just register service worker
+      } else 
         registerValidSW(swUrl, config);
-      }
+        // Is not localhost. Just register service worker
     });
   }
 }
@@ -60,9 +59,8 @@ function registerValidSW(swUrl, config) {
     .then(registration => {
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
-        if (installingWorker == null) {
+        if (installingWorker == null)
           return;
-        }
         installingWorker.onstatechange = () => {
           if (installingWorker.state === 'installed') {
             if (navigator.serviceWorker.controller) {
@@ -75,9 +73,9 @@ function registerValidSW(swUrl, config) {
               );
 
               // Execute callback
-              if (config && config.onUpdate) {
+              if (config && config.onUpdate) 
                 config.onUpdate(registration);
-              }
+              
             } else {
               // At this point, everything has been precached.
               // It's the perfect time to display a
@@ -85,9 +83,8 @@ function registerValidSW(swUrl, config) {
               console.log('Content is cached for offline use.');
 
               // Execute callback
-              if (config && config.onSuccess) {
+              if (config && config.onSuccess) 
                 config.onSuccess(registration);
-              }
             }
           }
         };
@@ -114,10 +111,10 @@ function checkValidServiceWorker(swUrl, config) {
             window.location.reload();
           });
         });
-      } else {
-        // Service worker found. Proceed as normal.
+      } else 
         registerValidSW(swUrl, config);
-      }
+        // Service worker found. Proceed as normal.
+        
     })
     .catch(() => {
       console.log(
@@ -127,9 +124,9 @@ function checkValidServiceWorker(swUrl, config) {
 }
 
 export function unregister() {
-  if ('serviceWorker' in navigator) {
+  if ('serviceWorker' in navigator) 
     navigator.serviceWorker.ready.then(registration => {
       registration.unregister();
     });
-  }
+  
 }
