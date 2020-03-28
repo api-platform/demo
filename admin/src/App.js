534ccab2fe371939e8d37d781f70255d57151cc1
Alan Poulain <contact@alanpoulain.eu>
2020-03-28 17:54:19 +0100

    [Admin] Add login (#147)
    * Add login
    
    * Access to login page directly
    
    * Prefill login page

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Redirect, Route } from "react-router-dom";
 import {
   HydraAdmin,
   ResourceGuesser,
@@ -7,21 +8,32 @@ import {
   CreateGuesser,
   EditGuesser,
   FieldGuesser,
-  InputGuesser
+  InputGuesser,
+  hydraDataProvider as baseHydraDataProvider,
+  fetchHydra as baseFetchHydra,
 } from "@api-platform/admin";
-import { ReferenceInput, AutocompleteInput } from "react-admin";
+import parseHydraDocumentation from "@api-platform/api-doc-parser/lib/hydra/parseHydraDocumentation";
+import { AutocompleteInput, ReferenceField, ReferenceInput, TextField } from "react-admin";
+import authProvider from "./authProvider";
+import Login from "./layout/Login";
 
-const ReviewsList = props => (
+const entrypoint = process.env.REACT_APP_API_ENTRYPOINT;
+
+const ReviewsList = (props) => (
   <ListGuesser {...props}>
     <FieldGuesser source="author" />
     <FieldGuesser source="book" />
+    {/* Use react-admin components directly when you want complex fields. */}
+    <ReferenceField label="Book's title" source="book" reference="books">
+      <TextField source="title" />
+    </ReferenceField>
 
     {/* While deprecated fields are hidden by default, using an explicit FieldGuesser component allows to add them back. */}
     <FieldGuesser source="letter" />
   </ListGuesser>
 );
 
-const ReviewsShow = props => (
+const ReviewsShow = (props) => (
   <ShowGuesser {...props}>
     <FieldGuesser source="author" addLabel={true} />
     <FieldGuesser source="book" addLabel={true} />
@@ -35,14 +47,15 @@ const ReviewsShow = props => (
   </ShowGuesser>
 );
 
-const ReviewsCreate = props => (
+const ReviewsCreate = (props) => (
   <CreateGuesser {...props}>
     <InputGuesser source="author" />
+    {/* Use react-admin components directly when you want complex inputs. */}
     <ReferenceInput
       source="book"
       reference="books"
       label="Books"
-      filterToQuery={searchText => ({ title: searchText })}
+      filterToQuery={(searchText) => ({ title: searchText })}
     >
       <AutocompleteInput optionText="title" />
     </ReferenceInput>
@@ -57,15 +70,16 @@ const ReviewsCreate = props => (
   </CreateGuesser>
 );
 
-const ReviewsEdit = props => (
+const ReviewsEdit = (props) => (
   <EditGuesser {...props}>
     <InputGuesser source="author" />
 
+    {/* Use react-admin components directly when you want complex inputs. */}
     <ReferenceInput
       source="book"
       reference="books"
       label="Books"
-      filterToQuery={searchText => ({ title: searchText })}
+      filterToQuery={(searchText) => ({ title: searchText })}
     >
       <AutocompleteInput optionText="title" />
     </ReferenceInput>
@@ -80,8 +94,63 @@ const ReviewsEdit = props => (
   </EditGuesser>
 );
 
+const fetchHeaders = () => ({
+  Authorization: `Bearer ${localStorage.getItem("token")}`,
+});
+const fetchHydra = (url, options = {}) =>
+  localStorage.getItem("token")
+    ? baseFetchHydra(url, {
+        ...options,
+        headers: new Headers(fetchHeaders()),
+      })
+    : baseFetchHydra(url, options);
+const apiDocumentationParser = (entrypoint) =>
+  parseHydraDocumentation(
+    entrypoint,
+    localStorage.getItem("token")
+      ? {
+          headers: new Headers(fetchHeaders()),
+        }
+      : {}
+  ).then(
+    ({ api }) => ({ api }),
+    (result) => {
+      // Only useful when the API endpoint is secured
+      if (result.status === 401) {
+        // Prevent infinite loop if the token is expired
+        localStorage.removeItem("token");
+        return Promise.resolve({
+          api: result.api,
+          customRoutes: [
+            <Route
+              path="/"
+              render={() =>
+                localStorage.getItem("token") ? (
+                  window.location.reload()
+                ) : (
+                  <Redirect to="/login" />
+                )
+              }
+            />,
+          ],
+        });
+      }
+      return Promise.reject(result);
+    }
+  );
+const dataProvider = baseHydraDataProvider(
+  entrypoint,
+  fetchHydra,
+  apiDocumentationParser
+);
+
 export default () => (
-  <HydraAdmin entrypoint={process.env.REACT_APP_API_ENTRYPOINT}>
+  <HydraAdmin
+    entrypoint={entrypoint}
+    dataProvider={dataProvider}
+    authProvider={authProvider}
+    loginPage={Login}
+  >
     <ResourceGuesser name="books" />
     <ResourceGuesser
       name="reviews"
---
+++ admin/src/authProvider.js
@@ -0,0 +1,37 @@
+export default {
+  login: ({ username, password }) => {
+    const request = new Request(
+      `${process.env.REACT_APP_API_ENTRYPOINT}/authentication_token`,
+      {
+        method: "POST",
+        body: JSON.stringify({ email: username, password }),
+        headers: new Headers({ "Content-Type": "application/json" }),
+      }
+    );
+    return fetch(request)
+      .then((response) => {
+        if (response.status < 200 || response.status >= 300) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then(({ token }) => {
+        localStorage.setItem("token", token);
+      });
+  },
+  logout: () => {
+    localStorage.removeItem("token");
+    return Promise.resolve();
+  },
+  checkAuth: () =>
+    localStorage.getItem("token") ? Promise.resolve() : Promise.reject(),
+  checkError: (error) => {
+    const status = error.status;
+    if (status === 401 || status === 403) {
+      localStorage.removeItem("token");
+      return Promise.reject();
+    }
+    return Promise.resolve();
+  },
+  getPermissions: () => Promise.resolve(),
+};
