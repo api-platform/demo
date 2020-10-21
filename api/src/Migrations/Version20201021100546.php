ceb34ece1c1c4a068816fcb8f8f4dd6710a6d7dc
Vincent Chalamon <vincentchalamon@gmail.com>
2020-10-21 12:07:10 +0200

    Update Review entity schema + regenerate migration

--- api/src/Migrations/Version20201021100546.php
+++ api/src/Migrations/Version20201021100546.php
@@ -7,7 +7,7 @@ namespace App\Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20201020134651 extends AbstractMigration
+final class Version20201021100546 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -18,17 +18,17 @@ final class Version20201020134651 extends AbstractMigration
     {
         $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE TABLE parchments (id UUID NOT NULL, title VARCHAR(255) NOT NULL, description TEXT NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('COMMENT ON COLUMN parchments.id IS \'(DC2Type:uuid)\'');
         $this->addSql('CREATE TABLE books (id UUID NOT NULL, isbn VARCHAR(255) DEFAULT NULL, title VARCHAR(255) NOT NULL, description TEXT NOT NULL, author VARCHAR(255) NOT NULL, publication_date DATE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN books.id IS \'(DC2Type:uuid)\'');
-        $this->addSql('CREATE TABLE reviews (id UUID NOT NULL, book_id UUID DEFAULT NULL, body TEXT DEFAULT NULL, rating SMALLINT NOT NULL, letter VARCHAR(255) DEFAULT NULL, author TEXT DEFAULT NULL, publication_date TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX IDX_6970EB0F16A2B381 ON reviews (book_id)');
-        $this->addSql('COMMENT ON COLUMN reviews.id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN reviews.book_id IS \'(DC2Type:uuid)\'');
         $this->addSql('CREATE TABLE users (id UUID NOT NULL, email VARCHAR(180) NOT NULL, password VARCHAR(255) NOT NULL, roles JSON NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_1483A5E9E7927C74 ON users (email)');
         $this->addSql('COMMENT ON COLUMN users.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE reviews (id UUID NOT NULL, book_id UUID NOT NULL, body TEXT NOT NULL, rating SMALLINT NOT NULL, letter VARCHAR(255) DEFAULT NULL, author TEXT DEFAULT NULL, publication_date TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_6970EB0F16A2B381 ON reviews (book_id)');
+        $this->addSql('COMMENT ON COLUMN reviews.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN reviews.book_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE parchments (id UUID NOT NULL, title VARCHAR(255) NOT NULL, description TEXT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN parchments.id IS \'(DC2Type:uuid)\'');
         $this->addSql('ALTER TABLE reviews ADD CONSTRAINT FK_6970EB0F16A2B381 FOREIGN KEY (book_id) REFERENCES books (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
@@ -37,9 +37,9 @@ final class Version20201020134651 extends AbstractMigration
         $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('ALTER TABLE reviews DROP CONSTRAINT FK_6970EB0F16A2B381');
+        $this->addSql('DROP TABLE books');
         $this->addSql('DROP TABLE users');
         $this->addSql('DROP TABLE reviews');
-        $this->addSql('DROP TABLE books');
         $this->addSql('DROP TABLE parchments');
     }
 }
