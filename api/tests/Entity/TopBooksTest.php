5b080c0178c9156e28b6ff5df6316bcf692bded9
Vernet Loïc <loic@les-tilleuls.coop>
2020-10-29 10:34:50 +0100

    tests: added collection related tests

---
+++ api/tests/Entity/TopBooksTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Entity;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\DataProvider\TopBookCollectionDataProvider;
+use App\Entity\TopBook;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+
+class TopBooksTest extends ApiTestCase
+{
+    // This trait provided by HautelookAliceBundle will take care of refreshing
+    // the database content to put it in a known state between every tests
+    use RefreshDatabaseTrait;
+
+    /**
+     * @see TopBookCollectionDataProvider::getCollection()
+     */
+    public function testGetCollection(): void
+    {
+        // The client implements Symfony HttpClient's `HttpClientInterface`, and the response `ResponseInterface`
+        $response = static::createClient()->request('GET', '/top_books');
+
+        self::assertResponseIsSuccessful();
+        self::assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        self::assertJsonContains([
+            '@context' => '/contexts/TopBook',
+            '@id' => '/top_books',
+            '@type' => 'hydra:Collection',
+            'hydra:totalItems' => 100,
+            'hydra:view' => [
+                '@id' => '/top_books?page=1',
+                '@type' => 'hydra:PartialCollectionView',
+                'hydra:first' => '/top_books?page=1',
+                'hydra:last' => '/top_books?page=4',
+                'hydra:next' => '/top_books?page=2',
+            ],
+        ]);
+
+        // 30 is the number returned by TopBookCollectionDataProvider::getItemsPerPage()
+        self::assertCount(30, $response->toArray()['hydra:member']);
+
+        // Checks that the returned JSON is validated by the JSON Schema generated for this API Resource by API Platform
+        // This JSON Schema is also used in the generated OpenAPI spec
+        self::assertMatchesResourceCollectionJsonSchema(TopBook::class);
+    }
+}
