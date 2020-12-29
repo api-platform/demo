83e44b1792e5d777879e8f79a70edc47da356e61
Vincent Chalamon <vincentchalamon@protonmail.com>
2020-12-29 09:47:40 +0100

    Fixes #184: update deprecations + fix HttpClient call typo

--- api/public/index.php
+++ api/public/index.php
@@ -18,7 +18,7 @@ if ($_SERVER['HTTP_CF_VISITOR'] ?? false) {
 }
 
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL);
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
 }
 
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
