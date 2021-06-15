bf646b86594adf6ed00d72f9675dc268c00d72d6
Alan Poulain <contact@alanpoulain.eu>
2021-06-15 18:28:48 +0200

    chore: bump dependencies

--- api/docker/php/docker-entrypoint.sh
+++ api/docker/php/docker-entrypoint.sh
@@ -55,13 +55,25 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 			echo "The database is now ready and reachable"
 		fi
 
+		if [ "$APP_ENV" != 'prod' ]; then
+			echo "Create test database"
+			bin/console doctrine:database:create --if-not-exists -e test
+		fi
+
 		if ls -A migrations/*.php >/dev/null 2>&1; then
-			php bin/console doctrine:migrations:migrate --no-interaction
+			echo "Execute migrations"
+			bin/console doctrine:migrations:migrate --no-interaction
+			if [ "$APP_ENV" != 'prod' ]; then
+				echo "Execute migrations (test)"
+				bin/console doctrine:migrations:migrate --no-interaction -e test
+			fi
 		fi
 
 		if [ "$APP_ENV" != 'prod' ]; then
-			echo "Load fixtures"
+			echo "Load fixtures (dev)"
 			bin/console hautelook:fixtures:load --no-interaction
+			echo "Load fixtures (test)"
+			bin/console hautelook:fixtures:load --no-interaction -e test
 		fi
 	fi
 fi
