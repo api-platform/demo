a9d6a2c988046e5c0f7d4ea45f123657b762d9d3
Alan Poulain <contact@alanpoulain.eu>
2021-06-21 20:08:29 +0200

    feat: use Doctrine attributes (#220)

--- api/src/Entity/Review.php
+++ api/src/Entity/Review.php
@@ -10,6 +10,7 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\Doctrine\UuidGenerator;
 use Ramsey\Uuid\UuidInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -18,9 +19,8 @@ use Symfony\Component\Validator\Constraints as Assert;
  * A review of an item - for example, of a restaurant, movie, or store.
  *
  * @see http://schema.org/Review Documentation on Schema.org
- *
- * @ORM\Entity
  */
+#[ORM\Entity]
 #[ApiResource(
     iri: 'http://schema.org/Review',
     mercure: true,
@@ -30,19 +30,14 @@ use Symfony\Component\Validator\Constraints as Assert;
 #[ApiFilter(OrderFilter::class, properties: ['id', 'publicationDate'])]
 class Review
 {
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="uuid", unique=true)
-     * @ORM\GeneratedValue(strategy="CUSTOM")
-     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
-     */
+    #[ORM\Id, ORM\GeneratedValue(strategy: 'CUSTOM'), ORM\CustomIdGenerator(class: UuidGenerator::class)]
+    #[ORM\Column(type: 'uuid', unique: true)]
     private ?UuidInterface $id = null;
 
     /**
      * The actual body of the review.
-     *
-     * @ORM\Column(type="text")
      */
+    #[ORM\Column(type: 'text')]
     #[ApiProperty(iri: 'http://schema.org/reviewBody')]
     #[Assert\NotBlank]
     #[Groups(groups: ['book:read', 'review:read', 'review:write'])]
@@ -50,9 +45,8 @@ class Review
 
     /**
      * A rating.
-     *
-     * @ORM\Column(type="smallint")
      */
+    #[ORM\Column(type: 'smallint')]
     #[Assert\NotBlank]
     #[Assert\Range(min: 0, max: 5)]
     #[Groups(groups: ['review:read', 'review:write'])]
@@ -60,20 +54,18 @@ class Review
 
     /**
      * DEPRECATED (use rating now): A letter to rate the book.
-     *
-     * @Assert\Choice({"a", "b", "c", "d"})
-     * @ORM\Column(type="string", nullable=true)
      */
+    #[ORM\Column(type: 'string', nullable: true)]
+    #[Assert\Choice(['a', 'b', 'c', 'd'])]
     #[ApiProperty(deprecationReason: 'Use the rating property instead')]
     #[Groups(groups: ['review:read', 'review:write'])]
     public ?string $letter = null;
 
     /**
      * The item that is being reviewed/rated.
-     *
-     * @ORM\ManyToOne(targetEntity=Book::class, inversedBy="reviews")
-     * @ORM\JoinColumn(nullable=false)
      */
+    #[ORM\ManyToOne(targetEntity: Book::class, inversedBy: 'reviews')]
+    #[ORM\JoinColumn(nullable: false)]
     #[ApiFilter(SearchFilter::class)]
     #[ApiProperty(iri: 'http://schema.org/itemReviewed')]
     #[Assert\NotNull]
@@ -82,18 +74,16 @@ class Review
 
     /**
      * The author of the review.
-     *
-     * @ORM\Column(type="text", nullable=true)
      */
+    #[ORM\Column(type: 'text', nullable: true)]
     #[ApiProperty(iri: 'http://schema.org/author')]
     #[Groups(groups: ['review:read', 'review:write'])]
     public ?string $author = null;
 
     /**
      * Publication date of the review.
-     *
-     * @ORM\Column(nullable=true, type="datetime")
      */
+    #[ORM\Column(type: 'datetime', nullable: true)]
     #[Groups(groups: ['review:read', 'review:write'])]
     public ?\DateTimeInterface $publicationDate = null;
 
