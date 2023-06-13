44f33de779b4f672829fe1f63d6886dde8f7e9b9
Paul-joseph Krogulec <paul-joseph@les-tilleuls.coop>
2023-06-13 18:25:26 +0200

    feat: add e2e tests with playwright
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright
    
    feat: add e2e tests with playwright

---
+++ pwa/tests/admin/hydra/TopBooks.spec.ts
@@ -0,0 +1,26 @@
+import {expect, test} from "@playwright/test";
+import {PageAdmin} from "../PageAdmin";
+
+test("Go to Admin Top Books Hydra", async ({browser}) => {
+  const Books = [
+    "Go to page 2",
+    "Go to page 10",
+    "Go to previous page",
+    "Go to page 1",
+  ]
+  const page = new PageAdmin(10,10, "/admin#/top_books")
+  await page.getAdminPage(browser)
+  await page.goToTopBooks()
+  for (const elts of Books) {
+    await page.changePage(elts)
+    await expect(await page.CountElsInList()).toEqual("10")
+  }
+})
+
+test("Go to Admin Top Books Show Hydra", async ({browser}) => {
+  const page = new PageAdmin(10,10, "/admin#/top_books")
+  await page.getAdminPage(browser)
+  await page.goToTopBooks()
+  await page.getElsClickable("link", "Show")
+  await expect((await page.getPages(browser)).url()).toContain("/show")
+})
