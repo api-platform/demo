dcee45345fc7c0a723434f5765e1e301f8857201
Vincent Chalamon <407859+vincentchalamon@users.noreply.github.com>
2023-09-11 12:25:02 +0200

    feat: redirect to /books on logout

--- pwa/utils/security.ts
+++ pwa/utils/security.ts
@@ -8,7 +8,7 @@ export async function signOut<R extends boolean = true>(
     options?: SignOutParams<R>
 ): Promise<R extends true ? undefined : SignOutResponse> {
   // @ts-ignore
-  const url = `${OIDC_SERVER_URL}/protocol/openid-connect/logout?id_token_hint=${session.idToken}&post_logout_redirect_uri=${options?.callbackUrl ?? window.location.href}`;
+  const url = `${OIDC_SERVER_URL}/protocol/openid-connect/logout?id_token_hint=${session.idToken}&post_logout_redirect_uri=${options?.callbackUrl ?? `${window.location.origin}/books`}`;
 
   return logout({ callbackUrl: url });
 }
