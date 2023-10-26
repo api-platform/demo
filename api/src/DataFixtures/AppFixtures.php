867c908b09fd7d921c9c97aea467e5865b40338e
Vincent Chalamon <407859+vincentchalamon@users.noreply.github.com>
2023-10-26 15:06:57 +0200

    fix: fixtures load

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -1,17 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\DataFixtures;
 
+use App\DataFixtures\Story\DefaultStory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
+final class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        DefaultStory::load();
     }
 }
