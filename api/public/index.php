12cffcf3b3f2a185a1a5e8758f9dd97f9c8b237d
Vincent Chalamon <407859+vincentchalamon@users.noreply.github.com>
2023-12-06 17:47:21 +0100

    chore: increase ingress proxy-buffer-size

--- api/public/index.php
+++ api/public/index.php
@@ -4,6 +4,12 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+// Support CloudFlare Flexible SSL
+// https://developers.cloudflare.com/fundamentals/reference/http-request-headers/#cf-visitor
+if ($_SERVER['HTTP_CF_VISITOR'] ?? false) {
+    $_SERVER['HTTP_X_FORWARDED_PROTO'] = \json_decode($_SERVER['HTTP_CF_VISITOR'], true)['scheme'];
+}
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
