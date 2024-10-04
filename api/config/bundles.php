05f065fedd966ef8da57e80663e4821b23883a95
Vincent Chalamon <407859+vincentchalamon@users.noreply.github.com>
2024-10-04 17:16:24 +0200

    chore: update api-platform to 4.0.3

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
