161951879707dbf419811e60a7233f985b2f9e7a
Vincent Chalamon <407859+vincentchalamon@users.noreply.github.com>
2025-02-28 12:39:06 +0100

    chore: update dependencies

--- api/tests/bootstrap.php
+++ api/tests/bootstrap.php
@@ -2,7 +2,11 @@
 
 declare(strict_types=1);
 
+use Faker\Factory as FakerFactory;
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\ErrorHandler;
+use Zenstruck\Foundry\Object\Instantiator;
+use Zenstruck\Foundry\Test\UnitTestConfig;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
@@ -15,3 +19,11 @@ if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 }
+
+// https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#non-kernel-tests
+UnitTestConfig::configure(instantiator: (Instantiator::withoutConstructor())
+    ->allowExtra()
+    ->alwaysForce(), faker: FakerFactory::create('en_GB'));
+
+// https://github.com/symfony/symfony/issues/53812#issuecomment-1962740145
+set_exception_handler([new ErrorHandler(), 'handleException']);
