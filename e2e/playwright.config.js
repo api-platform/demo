8cc67ebfad8208be4d4f69158903e1089d51e76f
Vincent Chalamon <407859+vincentchalamon@users.noreply.github.com>
2025-04-14 09:51:16 +0200

    chore: migrate E2E tests

---
+++ e2e/playwright.config.js
@@ -0,0 +1,38 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  timeout: 45000,
+  expect: {
+    timeout: 10000,
+  },
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: 0,
+  workers: process.env.CI ? 2 : undefined,
+  reporter: process.env.CI ? 'github' : 'line',
+  use: {
+    ignoreHTTPSErrors: true,
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    baseURL: 'https://localhost',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'], },
+      retries: 1,
+      fullyParallel: true,
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
