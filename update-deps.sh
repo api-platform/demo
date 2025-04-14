f529d1811dfee7bf895f70c137bb5e41ee8ded5f
Vincent Chalamon <407859+vincentchalamon@users.noreply.github.com>
2025-04-14 09:47:20 +0200

    chore: update structure from standard edition

--- update-deps.sh
+++ update-deps.sh
@@ -15,4 +15,5 @@ cd api
 composer recipes:update
 
 echo 'Run `git diff` and carefully inspect the changes made by the recipes.'
-echo 'Run `docker compose up --force-recreate` now and check that everything is fine!'
+echo 'Run `docker compose up --wait --force-recreate` now and check that everything is fine!'
+echo 'Run `docker compose exec php /bin/sh -c ''bin/console -e test doctrine:database:create ; bin/console -e test doctrine:migrations:migrate --no-interaction ; bin/phpunit ; bin/console -e test doctrine:schema:validate''` to check that the tests are green.'
