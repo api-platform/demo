3036b7a4d7557b13817c5033f329337fbf967343
Vincent Chalamon <407859+vincentchalamon@users.noreply.github.com>
2025-06-23 09:26:05 +0200

    fix: fix E2E tests for GraphQL

--- e2e/tests/GraphQL.spec.ts
+++ e2e/tests/GraphQL.spec.ts
@@ -3,6 +3,6 @@ import { expect, test } from "@playwright/test";
 test.describe("GraphQL", () => {
   test("Check GraphQL playground @read", async ({ page }) => {
     await page.goto("/graphql");
-    await expect(page.getByTestId("graphiql-container")).toContainText("Welcome to GraphiQL");
+    await expect(page.getByText("Welcome to GraphiQL")).toBeVisible();
   });
 })
